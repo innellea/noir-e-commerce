4c8d49e1899bab649f62d3148eca86867fa8e9fb
ROLAND KIRALY <43180177+toobig4u@users.noreply.github.com>
2021-04-26 17:17:35 +0200

    menu-item updated to navigate with React Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+// Dependencies
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+// Styles
 import "./App.css";
-import HomePage from "./pages/homepage/homepage.component"
+// Components
+import HomePage from "./pages/homepage/homepage.component";
+//
+
+const HatsPage = (props) => (
+  <div>
+    {console.log(props)}
+    <h1>Hats Page</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
