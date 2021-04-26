b1d5cd347aff702ff632162a930ae3906eceb858
ROLAND KIRALY <43180177+toobig4u@users.noreply.github.com>
2021-04-26 19:32:49 +0200

    adding header componment and links

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./App.css";
 // Components
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component.jsx"
+import Header from './components/header/header.component'
 //
 
 const HatsPage = (props) => (
@@ -18,6 +19,7 @@ const HatsPage = (props) => (
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
