7a0389f500fbf36f45ac631a1856e96024b22cd5
0xFFFF <43180177+innellea@users.noreply.github.com>
2021-07-24 19:21:22 +0200

    fix: cleanup

--- src/components/directory/directory.styles.jsx
+++ src/components/directory/directory.styles.jsx
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
 
-// eslint-disable-next-line import/prefer-default-export
 export const DirectoryMenuContainer = styled.div`
-  width: 100%;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
 `;
